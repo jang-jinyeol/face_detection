3f4c90f71b01aedc2ff306a4184788d6c1ece760
진열 장 <ornn@kw.ac.kr>
2021-12-20 02:17:19 +0900

    .

--- face_sdk/api_usage/verify.py
+++ face_sdk/api_usage/verify.py
@@ -9,6 +9,8 @@ logger = logging.getLogger('api')
 from core.model_loader.face_detection.FaceDetModelLoader import FaceDetModelLoader
 from core.model_handler.face_detection.FaceDetModelHandler import FaceDetModelHandler
 from core.model_loader.face_alignment.FaceAlignModelLoader import FaceAlignModelLoader
+from core.image_cropper.arcface_cropper.FaceRecImageCropper import FaceRecImageCropper
+
 from core.model_handler.face_alignment.FaceAlignModelHandler import FaceAlignModelHandler
 from torchvision.transforms.functional import to_pil_image
 import torchvision
@@ -82,32 +84,18 @@ if __name__ == '__main__':
 
     faceDetModelHandler = FaceDetModelHandler(model, 'cuda:0', cfg)
     faceAlignModelHandler = FaceAlignModelHandler(model2, 'cuda', cfg2)
+    # face_cropper = FaceRecImageCropper()
+
+    # t = transforms.Compose([transforms.ToTensor(),
+    #                         transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])])
 
 
-    # image = cv2.imread("C:/Users/ddcfd/Downloads/test/arin_label.jpg", cv2.IMREAD_COLOR)
+
+
+
+    image = cv2.imread("C:/Users/ddcfd/Downloads/test/arin_label.jpg", cv2.IMREAD_COLOR)
     # capp = cv2.VideoCapture("C:/Users/ddcfd/Downloads/test/arin_label.jpg", cv2.IMREAD_COLOR)
-    # cap2 = cv2.VideoCapture("http://192.168.0.75:4747/mjpegfeed?640x480")
-    # while cap2.isOpened():
-    #     # cv2.imshow("cap",cap2)
-    #     isSuccess,frame = cap2.read()
-    #     if isSuccess:
-    #         bboxs = faceDetModelHandler.inference_on_image(frame)
-    #         # det = np.asarray(list(map(int, bboxs[0:4])), dtype=np.int32)
-    #         image_show = frame.copy()
-    #         for box in bboxs:
-    #             det = np.asarray(list(map(int, box[0:4])), dtype=np.int32)
-    #             landmarks = faceAlignModelHandler.inference_on_image(frame, det)
-    #             for (x, y) in landmarks.astype(np.int32):
-    #                 cv2.circle(image_show, (x, y), 2, (255, 0, 0), -1)
-    #
-    #         # for box in bboxs:
-    #         #     box = list(map(int, box))
-    #         #     cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 2)
-    #         cv2.imshow("cap",image_show)
-    #
-    #     if cv2.waitKey(1) & 0xFF == ord('q'):
-    #         break
-
+    cap2 = cv2.VideoCapture("http://192.168.0.75:4747/mjpegfeed?640x480")
 
 
     image2=image.copy()
@@ -119,10 +107,10 @@ if __name__ == '__main__':
     for (x, y) in landmarks.astype(np.int32):
         cv2.circle(image2, (x, y), 2, (255, 0, 0), -1)
     kkk=faceAlignModelHandler.just_resize(image,det)
-    t = transforms.Compose([transforms.ToTensor(),
-                            transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])])
-    # t = transforms.Compose([transforms.ToTensor()])
-    # img_tensor = t(kkk).unsqueeze(0).to(device='cuda')
+    # t = transforms.Compose([transforms.ToTensor(),
+    #                         transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])])
+    t = transforms.Compose([transforms.ToTensor()])
+    # # img_tensor = t(kkk).unsqueeze(0).to(device='cuda')
     modela = Resnet(50, 0.4, 'ir').cuda()
     # modela.cuda()
     modela.eval()
@@ -139,10 +127,75 @@ if __name__ == '__main__':
     # print("embedding type: ", type(embedding))
     # print("embedding shape: ",embedding.shape)
     embeddings.append(embedding)
+    embeddings = torch.cat(embeddings)
+
     names.append("test")
     torch.save(embeddings,"C:/Users/ddcfd/Downloads/test/face.pth")
     names = np.array(names)
     np.save("C:/Users/ddcfd/Downloads/test/names",names)
+
+
+
+    def infer(faces,target_embs):
+        modela.eval()
+
+        embs2 = []
+
+        embs2.append(modela(t(faces).to(device='cuda').unsqueeze(0)))
+        source_embs = torch.cat(embs2)
+        diff = source_embs.unsqueeze(-1) - target_embs.transpose(1,0).unsqueeze(0)
+        dist = torch.sum(torch.pow(diff, 2), dim=1)
+        minimum, min_idx = torch.min(dist, dim=1)
+        min_idx[minimum > 1.5] = -1 # if no match, set idx to -1
+        return min_idx, minimum
+
+    def draw_box_name(bbox,name,frame):
+        frame = cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 6)
+        frame = cv2.putText(frame,
+                            name,
+                            (bbox[0], bbox[1]),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            2,
+                            (0, 255, 0),
+                            3,
+                            cv2.LINE_AA)
+        return frame
+
+
+
+
+
+    while cap2.isOpened():
+        # cv2.imshow("cap",cap2)
+        isSuccess,frame = cap2.read()
+        if isSuccess:
+            try:
+                bboxs = faceDetModelHandler.inference_on_image(frame)
+                bboxs = bboxs.astype(int)
+                # det = np.asarray(list(map(int, bboxs[0:4])), dtype=np.int32)
+                # image_show = frame.copy()
+                for idx,box in enumerate(bboxs):
+                    det = np.asarray(list(map(int, box[0:4])), dtype=np.int32)
+                    landmarks = faceAlignModelHandler.inference_on_image(frame, det)
+                    r_frame = faceAlignModelHandler.just_resize(frame, det)
+                    results, score = infer(r_frame,embeddings)
+                    # frame = draw_box_name(box, names[results[idx] + 1], frame)
+                    frame = draw_box_name(box, names[results[idx] + 1] + '_{:.2f}'.format(score[idx]), frame)
+
+                    # for (x, y) in landmarks.astype(np.int32):
+                    #     cv2.circle(image_show, (x, y), 2, (255, 0, 0), -1)
+
+                # for box in bboxs:
+                #     box = list(map(int, box))
+                #     cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 2)
+            except:
+                print("detect error")
+            cv2.imshow("cap",frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
 
 
 
